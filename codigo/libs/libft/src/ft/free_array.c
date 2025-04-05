c22349d568121eb9b3ca971bdad50a53c170f0ac
briveraarg <brivera@student.42madrid.com>
2025-04-05 20:20:52 +0200

    correccion de funcion free array

--- codigo/libs/libft/src/ft/free_array.c
+++ codigo/libs/libft/src/ft/free_array.c
@@ -3,19 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   free_array.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: brivera <brivera@student.42madrid.com>     +#+  +:+       +#+        */
+/*   By: brivera@student.42madrid.com <brivera>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/27 10:59:13 by brivera           #+#    #+#             */
-/*   Updated: 2025/03/27 10:59:40 by brivera          ###   ########.fr       */
+/*   Updated: 2025/04/05 18:55:09 by brivera@stu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**free_array(char **array)
+/**
+ * Frees a NULL-terminated array of strings and the array itself.
+ * 
+ * @param array The array to free (may be NULL)
+ * @return Always returns NULL to allow convenient assignment in calling code
+ *         Example usage: array = free_array(array);
+ * 
+ * @note This function safely handles NULL input and properly frees all
+ *       allocated strings in the array before freeing the array pointer.
+ *       After calling, the array pointer should be set to NULL to avoid
+ *       dangling references.
+ */
+
+void	*free_array(char **array)
 {
 	size_t	i;
 
+	if (!array)
+		return (NULL);
 	i = 0;
 	while (array[i] != NULL)
 	{
