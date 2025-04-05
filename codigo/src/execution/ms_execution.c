3908cb09af3338febd6eab4b7acc8bd57fabfdf2
briveraarg <brivera@student.42madrid.com>
2025-04-05 20:22:26 +0200

    que devuelva el codigo de exit

--- codigo/src/execution/ms_execution.c
+++ codigo/src/execution/ms_execution.c
@@ -6,13 +6,37 @@
 /*   By: brivera@student.42madrid.com <brivera>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 18:15:12 by brivera           #+#    #+#             */
-/*   Updated: 2025/04/04 21:44:04 by brivera@stu      ###   ########.fr       */
+/*   Updated: 2025/04/05 19:47:35 by brivera@stu      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ms_exec_builtin_or_other(char **command, t_mshell *data)
+int	ms_echo(void)
+{
+	printf("echo\n");
+	return (0);
+}
+
+int	ms_cd(void)
+{
+	printf("cd\n");
+	return (0);
+}
+
+int	ms_pwd(void)
+{
+	printf("pwd\n");
+	return (0);
+}
+
+int	ms_exec_other(void)
+{
+	printf("demas comandos\n");
+	return (0);
+}
+
+int	ms_exec_builtin_or_other(char **command, t_mshell *data)
 {
 	if (!ft_strncmp(command[0], "echo", 5))
 		ms_echo();
@@ -21,13 +45,14 @@ void	ms_exec_builtin_or_other(char **command, t_mshell *data)
 	else if (!ft_strncmp(command[0], "pwd", 4))
 		ms_pwd();
 	else if (!ft_strncmp(command[0], "export", 7))
-		data->exits->code = ms_export(command, data);
+		data->exits = ms_export(command, data);
 	else if (!ft_strncmp(command[0], "unset", 6))
 		ms_unset();
 	else if (!ft_strncmp(command[0], "env", 4))
-		data->exits->code = ms_env(command, data->env);
+		data->exits = ms_env(command, &data->env);
 	else if (!ft_strncmp(command[0], "exit", 5))
 		ms_exit();
 	else
-		ms_exec_other(command, data);
+		ms_exec_other();
+	return (0);
 }
