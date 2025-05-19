268d0a0541fa04b9b98f824b8397945cfbbf6742
FreddyRivasVE <frivas@student.42madrid.com>
2025-05-19 13:16:54 +0200

    testing all

--- codigo/src/parce/ms_redir_together.c
+++ codigo/src/parce/ms_redir_together.c
@@ -6,7 +6,7 @@
 /*   By: frivas <frivas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/25 10:42:24 by frivas            #+#    #+#             */
-/*   Updated: 2025/05/03 13:25:46 by frivas           ###   ########.fr       */
+/*   Updated: 2025/05/19 12:50:23 by frivas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,8 @@ static int	ms_calculate_new_length(char *str)
 		double_quote = toggle_doubles(str[i], single_quote, double_quote);
 		if (!single_quote && !double_quote && ft_isredirection_char(str[i]))
 		{
+			if (i > 0 && !ft_isspace(str[i - 1]))
+				extra_spaces++;
 			if (ft_isredirection_char(str[i + 1]))
 				i++;
 			if (!ft_isspace(str[i + 1]) && str[i + 1] != '\0')
@@ -39,29 +41,39 @@ static int	ms_calculate_new_length(char *str)
 	return (i + extra_spaces);
 }
 
+static void	handle_redirection_chars(char *str, char *new_str, int *i, int *j)
+{
+	if (ft_isredirection_char(str[(*i)]))
+	{
+		if (ft_isredirection_char(str[(*i) + 1]))
+			new_str[++(*j)] = str[++(*i)];
+		if (!ft_isspace(str[(*i) + 1]) && str[(*i) + 1] != '\0')
+			new_str[++(*j)] = ' ';
+	}
+}
+
 static void	ms_build_new_string(char *str, char *new_str)
 {
 	int		i;
 	int		j;
-	bool	single_quote;
-	bool	double_quote;
+	bool	sq;
+	bool	dq;
 
 	i = 0;
 	j = 0;
-	single_quote = false;
-	double_quote = false;
+	sq = false;
+	dq = false;
 	while (str[i])
 	{
-		single_quote = toggle_simples(str[i], single_quote, double_quote);
-		double_quote = toggle_doubles(str[i], single_quote, double_quote);
+		sq = toggle_simples(str[i], sq, dq);
+		dq = toggle_doubles(str[i], sq, dq);
 		new_str[j] = str[i];
-		if (!single_quote && !double_quote && ft_isredirection_char(str[i]))
-		{
-			if (ft_isredirection_char(str[i + 1]))
-				new_str[++j] = str[++i];
-			if (!ft_isspace(str[i + 1]) && str[i + 1] != '\0')
-				new_str[++j] = ' ';
-		}
+		if (str[i + 1] != '\0' && !sq && !dq
+			&& ft_isredirection_char(str[i + 1])
+			&& !ft_isredirection_char(str[i]) && !ft_isspace(str[i]))
+			new_str[++j] = ' ';
+		if (!sq && !dq && ft_isredirection_char(str[i]))
+			handle_redirection_chars(str, new_str, &i, &j);
 		i++;
 		j++;
 	}
