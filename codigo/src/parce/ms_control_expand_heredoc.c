dc4774b02a7dbd0c19b65a3d9e160e431a44e045
briveraarg <brivera@student.42madrid.com>
2025-05-26 17:26:22 +0200

    heredoc

--- codigo/src/parce/ms_control_expand_heredoc.c
+++ codigo/src/parce/ms_control_expand_heredoc.c
@@ -6,35 +6,52 @@
 /*   By: brivera <brivera@student.42madrid.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/17 15:29:32 by frivas            #+#    #+#             */
-/*   Updated: 2025/05/22 09:13:44 by brivera          ###   ########.fr       */
+/*   Updated: 2025/05/26 17:15:12 by brivera          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static bool	ms_heredoc_expand_aux(char *str, int pos, int i)
+{
+	int	j;
+	int	k;
+
+	j = i + 1;
+	while (str[j] && ft_isspace(str[j]))
+		j++;
+	if (j <= pos)
+	{
+		k = j;
+		while (k < pos && !ft_isspace(str[k]))
+			k++;
+		if (k >= pos)
+			return (true);
+	}
+	return (false);
+}
+
 bool	ms_control_expand_hered(char *str, int pos, t_mshell *data)
 {
 	int		i;
-	bool	dobleq;
+	bool	heredoc;
+	bool	dquote;
 
-	dobleq = data->flag;
-	i = 0;
-	if (pos > 0)
-		i = pos - 1;
-	if (!ft_isspace(str[i]))
+	i = pos;
+	heredoc = false;
+	dquote = data->flag;
+	while (i >= 0)
 	{
-		while (i >= 0 && !ft_isspace(str[i]))
-			i--;
-	}
-	while (i >= 0 && (ft_isspace(str[i]) || str[i] == '\"'))
-	{
-		if (str[i] == '\"' && dobleq)
-			dobleq = false;
-		else if (str[i] == '\"' && !dobleq)
-			dobleq = true;
+		if (i > 0 && str[i] == '<' && str[i - 1] == '<' && !dquote)
+		{
+			heredoc = true;
+			break ;
+		}
+		if (str[i] == '\"')
+			dquote = !dquote;
 		i--;
 	}
-	if (i > 0 && str[i] == '<' && str[i - 1] == '<' && dobleq == 0)
-		return (true);
+	if (heredoc)
+		return (ms_heredoc_expand_aux(str, pos, i));
 	return (false);
 }
